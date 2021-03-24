50a44c74e077be9d4925f31ca1d04b163ba97d0c
kaly <1018519785@qq.com>
2021-03-24 16:48:45 +0800

    fifth_commit

---
+++ word_in_string.py
@@ -0,0 +1,15 @@
+class Soluttion:
+    def countSegments(self, s):
+        res = 0
+        for i in range(len(s)):
+            if s[i] != ' ' and (i == 0 or s[i - 1] == ' '):
+                res += 1
+
+            return res
+
+
+if __name__ == '__main__':
+    s = Soluttion()
+    n = "hello, my name is DJ"
+    print("输入:", n)
+    print("输出:", s.countSegments(n))
